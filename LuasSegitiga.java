dae399ec19a7c453a387befb7c13f6bad8a56b71
Nazwa Rizqil kausar <nazwaaaaaaar@gmail.com>
2024-09-21 16:54:42 +0700

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,11 @@
+package tipedata;
+
+public class LuasSegitiga {
+    public static void main(String[] args) {
+        double alas = 8;
+        double tinggi = 4;
+        double luas = alas * tinggi / 2;
+        System.out.println("Luas Segitiga:" + luas);
+
+    }
+}
